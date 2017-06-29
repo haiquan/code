ee2d9d54364cef16090f0fd032f0e4df0288817f
Hayden <ourzhucez@163.com>
2017-06-29 21:22:08 +0800

    Add C# code.

---
+++ c#/ClrViaC/ClrViaC/Chapter5/TestType.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClrViaC.Chapter5
+{
+    public class TestType
+    {
+        public static void P114()
+        {
+            Int32 v = 5;
+            Object o = v;
+            v = 123;
+
+            Object o2;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                o2 = v + ", " + (Int32)o;
+                //Trace.TraceInformation(o2.ToString());
+            }
+            watch.Stop();
+            Console.WriteLine("spend time is {0}", watch.ElapsedMilliseconds);
+
+            watch.Start();
+            for (int i = 0; i < 10000; i++)
+            {
+                o2 = v + ", " + o;
+                //Trace.TraceInformation(o2.ToString());
+            }
+            watch.Stop();
+            Console.WriteLine("spend time is {0}", watch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ c#/ClrViaC/ClrViaC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClrViaC.Chapter5;
+
+namespace ClrViaC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestType.P114();
+            Console.ReadKey();
+        }
+    }
+}
